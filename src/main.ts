cf0e79e3c4c7b2ad5a3a977e5e6ba3bcad038194
ChoiYongWon <yongwon0824@naver.com>
2021-07-29 11:50:09 +0900

    :zap:Add Cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin : "54.180.166.3",
+      credentials: true
+    }
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
